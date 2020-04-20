c8188dae7abe1053b3d6e5c8c5dcf4e7f5d1f0c5
Sam Davies <sam@samueldavies.net>
2020-04-20 10:31:29 +0100

    Fix migration test

--- core/store/migrations/migrate_test.go
+++ core/store/migrations/migrate_test.go
@@ -1,8 +1,10 @@
 package migrations_test
 
 import (
+	"database/sql"
 	"fmt"
 	"math/big"
+	"net/url"
 	"os"
 	"testing"
 	"time"
@@ -31,8 +33,9 @@ func bootstrapORM(t *testing.T) (*orm.ORM, func()) {
 	config := tc.Config
 
 	require.NoError(t, os.MkdirAll(config.RootDir(), 0700))
-	require.NoError(t, cltest.GlobalPrepareTestDB(tc))
-	orm, err := orm.NewORM(config.DatabaseURL(), config.DatabaseTimeout(), gracefulpanic.NewSignal())
+	migrationTestDBURL, err := dropAndCreateMigrationTestDB(tc.DatabaseURL())
+	require.NoError(t, err)
+	orm, err := orm.NewORM(migrationTestDBURL, config.DatabaseTimeout(), gracefulpanic.NewSignal())
 	require.NoError(t, err)
 	orm.SetLogging(true)
 
@@ -41,6 +44,35 @@ func bootstrapORM(t *testing.T) (*orm.ORM, func()) {
 		cleanup()
 		os.RemoveAll(config.RootDir())
 	}
+}
+
+func dropAndCreateMigrationTestDB(databaseURL string) (string, error) {
+	parsed, err := url.Parse(databaseURL)
+	if err != nil {
+		return "", err
+	}
+
+	dbname := fmt.Sprintf("%s_migrations", parsed.Path[1:])
+	// Cannot drop test database if we are connected to it, so we must connect
+	// to a different one. template1 should be present on all postgres installations
+	parsed.Path = "/template1"
+	db, err := sql.Open(string(orm.DialectPostgres), parsed.String())
+	if err != nil {
+		return "", fmt.Errorf("unable to open postgres database for creating test db: %+v", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %s", dbname))
+	if err != nil {
+		return "", fmt.Errorf("unable to drop postgres migrations test database: %v", err)
+	}
+	// `CREATE DATABASE $1` does not seem to work w CREATE DATABASE
+	_, err = db.Exec(fmt.Sprintf("CREATE DATABASE %s", dbname))
+	if err != nil {
+		return "", fmt.Errorf("unable to create postgres migrations test database: %v", err)
+	}
+	parsed.Path = fmt.Sprintf("/%s", dbname)
+	return parsed.String(), nil
 }
 
 func TestMigrate_Migrations(t *testing.T) {
